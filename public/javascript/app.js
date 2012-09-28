3993284c015dce085a9479d229650102581146cd
pornthipw <pornthip.wong@gmail.com>
2012-09-28 16:23:30 +0700

    update pagination

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,13 +3,13 @@ var app = angular.module('gradfile', ['file_service']);
 app.config(function($routeProvider) {
     $routeProvider.
 	//when('/', {controller:FileController, templateUrl:'/static/index.html'}).    
-	when('/upload', {controller:UploadController, templateUrl:'/static/upload.html'}).
+	//when('/upload', {controller:UploadController, templateUrl:'/static/upload.html'}).
     when('/list', {controller:FileController, templateUrl:'/static/index.html'})
 });
-
+/*
 function UploadController($scope, $routeParams,FileDB) {  
     console.log('Execute');
-    
+
     $('iframe#upload_target').load(function() {
         var data = $.parseJSON($('iframe#upload_target').contents().find("body")[0].innerHTML);
         if(data.success) {
@@ -31,9 +31,47 @@ function UploadController($scope, $routeParams,FileDB) {
     };
     
 };
+*/
 
-function FileController($scope,FileDB){
-    $scope.file_list = FileDB.query();    
+function FileController($scope,$routeParams,FileDB){
+    console.log('Execute');            
+    
+    $scope.showFormPage = false;
+    $scope.viewForm = function() {
+        $scope.showFormPage = true;
+    };
+    
+    $('iframe#upload_target').load(function() {
+        var data = $.parseJSON($('iframe#upload_target').contents().find("body")[0].innerHTML);
+        if(data.success) {
+            $scope.$apply(function(){
+                $scope.success = true;
+                $scope.theFile = '';
+                $('#uploadFileModal').modal('hide')
+                $scope.file_list = FileDB.query();  
+            });
+        } else {
+            $scope.$apply(function() {
+                $scope.success = false;
+                $scope.message = data.message;
+            });
+        }
+    }
+    
+    );      
+
+    $scope.setFile = function(element) {
+        $scope.$apply(function() {
+            $scope.theFile = element.files[0];
+        });
+    };
+    
+    $scope.file_list = FileDB.query();
+    
+    
+    
+    
+        
     $scope.del = function(id) {
         console.log(id);
         FileDB.remove({id:id}, function(docs) {
@@ -41,8 +79,27 @@ function FileController($scope,FileDB){
             $scope.file_list = FileDB.query();    
         });
     };
+    
+    
+    $scope.currentPage = 0;
+    $scope.pageSize = 2;
+    $scope.numberOfPages=function(){
+        var totalPage = Math.ceil($scope.file_list.length/$scope.pageSize);       
+        return totalPage;          
+    }
+    for (var i=0; i<45; i++) {
+        $scope.file_list.push("File "+i);
+    }
+    
+
 };
 
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
 
 
 
